88c1e3b084d5d367f48c18913087544066e7eb21
Athavan <athavan63@live.ca>
2019-02-20 17:04:24 -0500

    Fixed toString method

--- 01/Solution.java
+++ 01/Solution.java
@@ -208,13 +208,29 @@ public class Solution {
      */
     public String toString() {
  
-        String aString = "";
-          for(int row = 0; row < gameArray.length; row++) {
-             for(int col = 0; col < gameArray[row].length; col++) {
-                    aString += " " + gameArray[row][col];
-             }
-          }
-          return "[" + aString + "]";        
+    	String array_output = "[[";
+    		for (int row = 0; row < gameArray.length; row++) {
+    			if (row>0) {
+    				array_output += "[";
+    			}
+    			
+    			for(int column = 0; column < gameArray[row].length; column++) {
+    				if (column < gameArray[row].length - 1) {
+    					array_output += gameArray[row][column] + ", "; 
+    				}
+    				else {
+    					array_output += gameArray[row][column];
+    				}
+    				
+    			}
+    			
+    			if (row != gameArray.length - 1) {
+    				array_output += ("], " + System.lineSeparator());
+    			}
+    			
+    		}
+    		array_output += "]]";
+    		return array_output;
     }
 
 }
